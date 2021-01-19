cbc4b9392cbf987167672c7b2b14bc0d0e08c4d3
Gabriela Abreu de Araujo <56653762+gabsabreu@users.noreply.github.com>
2021-01-18 21:48:46 -0300

    parâmetros range: (start, stop, [step])

---
+++ aula05ex2.py
@@ -0,0 +1,6 @@
+#A função range possui os seguintes parâmetros:
+#range(start, stop, [step])
+#Onde o step é opcional. Como queremos "pular" de 3 em 3, começando com 1 (start) até 10 (stop), podemos escrever:
+
+for contador in range(1,31,3):
+    print(contador)
